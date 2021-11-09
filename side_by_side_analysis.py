01a59a71011de95d9a190e94c70f480ec638e7d3
Jonwhear <93143577+Jonwhear@users.noreply.github.com>
2021-11-09 17:30:18 -0600

    Added while loop to make long-form data

--- side_by_side_analysis.py
+++ side_by_side_analysis.py
@@ -34,12 +34,24 @@ df_3 = pd.concat([df_new,df_2_new])
 #Rename the channels into something easier to digest for now
 df_3.rename({'IL1-IL2 - BLA5-BLA6': 'CH1', 'IL1-IL2 - BLA7-BLA8': 'CH2','IL3-IL4 - BLA5-BLA6':'CH3','IL3-IL4 - BLA7-BLA8':'CH4','IL5-IL6 - BLA5-BLA6':'CH5','IL5-IL6 - BLA7-BLA8':'CH6','IL7-IL8 - BLA5-BLA6':'CH7','IL7-IL8 - BLA7-BLA8':'CH8'}, axis=1, inplace=True)
 df_3['Freq'] = df_3.index # The Frequency (at least in my code) was getting saved to the index, this will create a new column
-ch_num = 8 # This it the number of channels being analyzed in the pre/post code
+
+ch_num = 1 # Number of channels - 8 by default. This defines number of while loops
+loop_num = ch_num +1 # This is to make sure we can put in the channel number literally and still get the loop to run the correct number of times
+m = 1
 
 # Seaborn will not let us use date in the "wide" format, and we will need to break it out into a "long" format
-# I think we'll have to do this seperately for each of the 8 channels - maybe using a while loop?
+# While loop needs to be run x number of times, and then the append the final df for analysis
 # After all 8 have been melted then we'll have to find a way to add delta data to the df as well, probably a "delta" in the "period" column
+# It might end up being easier getting deltas before the while loop, but cross that bridge when we get there
 
-df_temp_demo = df_3[['Freq','CH1','period']]
-# print(df_temp_demo.head(1))
-df_demo = pd.melt(df_temp_demo,id_vars=['CH1'],value_name=['CH1coh'])
+while m < loop_num:
+    channel = 'CH'+str(m)
+    # print(channel)
+    # value = channel+'value'
+    df_loop = df_3[['freq',channel,'period']]
+    df_loop['channel']=channel
+    df_loop.rename({channel: 'coh'},axis=1, inplace=True)
+    # rename the CH1/etc channel to 'coh' - this way they can all be appended together in the future
+    print(df_loop.head())
+    m +=1
+    # print('Channel is: '+channel)
