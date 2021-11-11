c1c8dfdca3e248f705cb3dd1c1d8cecf8e7b3809
Jonwhear <93143577+Jonwhear@users.noreply.github.com>
2021-11-11 09:52:58 -0600

    Updated SBS analysis - now working
    Now creates side by side graphs (using Rae's sns plts) of pre/post stimulation coherence from .mat pathway outputs
    
    I think this version will be good enough to move to the main analysis pipeline for further tweaking.

--- side_by_side_analysis.py
+++ side_by_side_analysis.py
@@ -16,7 +16,6 @@ df_2 = pd.read_csv('RAW_POST_dev2107_day1_TFR_coh.csv',index_col=False)
 df = df.T
 df_2 = df_2.T
 
-
 # Remove index and save to new DataFrame
 df.columns = df.iloc[0]
 df_new = df[1:]
@@ -24,34 +23,57 @@ df_new = df[1:]
 df_2.columns = df.iloc[0]
 df_2_new = df_2[1:]
 
-# Add deliniator to end of Data Frame - to deliniate if this datapoint is before or after stimulation
+# Add deliniator to end of Data Frame
 df_new['period'] = 'pre'
-df_2_new['period'] = 'post'
+df_2_new['period'] = 'post' bele
 
-# Pandas Concatination - Combine both Data Frames
+# Pandas Concatination
 df_3 = pd.concat([df_new,df_2_new])
+
+pd.set_option("display.max_rows", None)
 
 #Rename the channels into something easier to digest for now
 df_3.rename({'IL1-IL2 - BLA5-BLA6': 'CH1', 'IL1-IL2 - BLA7-BLA8': 'CH2','IL3-IL4 - BLA5-BLA6':'CH3','IL3-IL4 - BLA7-BLA8':'CH4','IL5-IL6 - BLA5-BLA6':'CH5','IL5-IL6 - BLA7-BLA8':'CH6','IL7-IL8 - BLA5-BLA6':'CH7','IL7-IL8 - BLA7-BLA8':'CH8'}, axis=1, inplace=True)
-df_3['Freq'] = df_3.index # The Frequency (at least in my code) was getting saved to the index, this will create a new column
 
-ch_num = 1 # Number of channels - 8 by default. This defines number of while loops
+# Pull frequency data from the index and create new 'freq' column
+df_3['freq'] = df_3.index
+
+# Remove the frequency data from the index
+df_3.reset_index(drop=True, inplace=True)
+# Remove index name from df_3
+# Drop first column of dataframe
+df_3 = df_3[['freq','CH1','CH2','CH3','CH4','CH5','CH6','CH7','CH8','period']]
+# print(df_3.head())
+
+ch_num = 8 # Number of channels - 8 by default. This defines number of while loops
 loop_num = ch_num +1 # This is to make sure we can put in the channel number literally and still get the loop to run the correct number of times
 m = 1
 
-# Seaborn will not let us use date in the "wide" format, and we will need to break it out into a "long" format
-# While loop needs to be run x number of times, and then the append the final df for analysis
-# After all 8 have been melted then we'll have to find a way to add delta data to the df as well, probably a "delta" in the "period" column
-# It might end up being easier getting deltas before the while loop, but cross that bridge when we get there
+# Build df_final before while loop
+column_list = ['freq','coh','period','channel']
+global df_final
+df_final = pd.DataFrame(columns=column_list)   # print(channel)
 
+# while loop to create final DataFrame (df_final)
 while m < loop_num:
     channel = 'CH'+str(m)
-    # print(channel)
-    # value = channel+'value'
     df_loop = df_3[['freq',channel,'period']]
     df_loop['channel']=channel
     df_loop.rename({channel: 'coh'},axis=1, inplace=True)
-    # rename the CH1/etc channel to 'coh' - this way they can all be appended together in the future
-    print(df_loop.head())
     m +=1
-    # print('Channel is: '+channel)
+    df_final = df_final.append(df_loop)
+    del df_loop
+
+# df_final postprocessing
+df_final = df_final.reset_index()
+df_final = df_final[['freq','coh','channel','period']]
+
+# Prepare and print Seaborn plot
+plot = sns.relplot(data =df_final,x='freq',y='coh',kind='line',col='period',hue='channel')
+plot.set_xlabels('freq')
+plot.set_ylabels('coh')
+plot.axes[0][0].set_xticks(range(10,61,10))
+plot.axes[0][0].set_xticklabels([10, 20, 30, 40, 50, 60])
+plt.axvline(8,0,0.9)
+plt.axvline(4,0,0.9)
+plt.show()
