f3b5887e1d536e6f1744354a76cf22cbe6584209
jonwhear <jonwhear@gmail.com>
2021-11-11 17:41:14 -0600

    Entrys save inputs when button is pressed

--- simple_GUI.py
+++ simple_GUI.py
@@ -1,4 +1,4 @@
-# Tkinter demo - 04Nov2021
+# Tkinter demo - Created 04Nov2021, updated 11Nov2021
 
 from tkinter import *
 
@@ -13,11 +13,19 @@ label_1 = Label(root,text="Day of Recording (Ex: day1)").place(x=20,y=20)
 label_2 = Label(root,text="Animal Number (Ex: 2207)").place(x=20,y=60)
 
 # Add Text
-entry_1 = Entry(root,width=20).place(x=220,y=20)
-entry_2 = Entry(root,width=20).place(x=220,y=60)
+r_day = StringVar()
+entry_1 = Entry(root,width=20,textvariable=r_day).place(x=220,y=20)
+a_num = StringVar()
+entry_2 = Entry(root,width=20,textvariable=a_num).place(x=220,y=60)
+
+def save_values():
+    rec_day = r_day.get()
+    ani_num = a_num.get()
+    print('Day: '+str(rec_day)+', Rat: '+str(ani_num))
+    print('run '*3)
 
 # Add Buttons
-btn = Button(root, text='Start Analysis').place(x=160,y=110)
+btn = Button(root, text='Start Analysis',command=save_values).place(x=160,y=110)
 
 # Title the Popup
 root.title('TNE Lab Coherence vs. Frequency Grapher')  # This is how you name the program window
