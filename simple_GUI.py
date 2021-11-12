83ffd59dd28759834061cdd4f283354eafaeb08b
Jonwhear <93143577+Jonwhear@users.noreply.github.com>
2021-11-12 14:00:38 -0600

    Reformatted into Class: graph_gui
    Prepped for usage in the main analysis script by creating graph_gui class

--- simple_GUI.py
+++ simple_GUI.py
@@ -1,37 +1,33 @@
-# Tkinter demo - Created 04Nov2021, updated 11Nov2021
+# Tkinter demo - Created 04Nov2021, updated 12Nov2021
 
 from tkinter import *
 
-# Create the main, "root", window
+class graph_gui:
+    def __init__ (self,root):
+        # Setup GUI Window
+        self.root = root
+        root.title('TNE Lab Coherence vs. Frequency Grapher') # Set Window Name
+        root.geometry('450x160') # Set Width vs. Height
+        root.resizable(width=False,height=False) # Fix W/H
+
+        # Add Labels
+        self.label_1 = Label(root,text="Day of Recording (Ex: day1)").place(x=20,y=20)
+        self.label_2 = Label(root,text="Animal Number (Ex: 2207)").place(x=20,y=60)
+
+        # Add Button
+        self.btn = Button(root, text='Start Analysis',command=self.save_values).place(x=160,y=110)
+
+        # Add and configure Entrys
+        self.r_day = StringVar()
+        self.a_num = StringVar()
+        self.entry_1 = Entry(root,width=20,textvariable=self.r_day).place(x=220,y=20)
+        self.entry_2 = Entry(root,width=20,textvariable=self.a_num).place(x=220,y=60)
+
+    def save_values(self):
+        rec_day = self.r_day.get()
+        ani_num = self.a_num.get()
+        print('Day: '+str(rec_day)+', Rat: '+str(ani_num))
+
 root = Tk()
-
-# Set Window Size
-root.geometry('450x160') # Width vs. Height
-
-# Add Labels
-label_1 = Label(root,text="Day of Recording (Ex: day1)").place(x=20,y=20)
-label_2 = Label(root,text="Animal Number (Ex: 2207)").place(x=20,y=60)
-
-# Add Text
-r_day = StringVar()
-entry_1 = Entry(root,width=20,textvariable=r_day).place(x=220,y=20)
-a_num = StringVar()
-entry_2 = Entry(root,width=20,textvariable=a_num).place(x=220,y=60)
-
-def save_values():
-    rec_day = r_day.get()
-    ani_num = a_num.get()
-    print('Day: '+str(rec_day)+', Rat: '+str(ani_num))
-    print('run '*3)
-
-# Add Buttons
-btn = Button(root, text='Start Analysis',command=save_values).place(x=160,y=110)
-
-# Title the Popup
-root.title('TNE Lab Coherence vs. Frequency Grapher')  # This is how you name the program window
-
-# Lock Window Size
-root.resizable(width=False, height=False)
-
-# Run mainloop()
-root.mainloop() # This tells the program to run and keep displaying
+tnel_gui = graph_gui(root)
+root.mainloop()
