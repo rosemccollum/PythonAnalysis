2906c55c818e0721a731db4aa40d1d0da9644300
Jonwhear <93143577+Jonwhear@users.noreply.github.com>
2021-11-15 15:08:42 -0600

    GUI now closes when finished et al.
    GUI closes when button is clicked, and some minor changes to user output text

--- simple_GUI.py
+++ simple_GUI.py
@@ -1,4 +1,4 @@
-# Tkinter demo - Created 04Nov2021, updated 12Nov2021
+# Simple tkinter GUI- Created 04Nov2021, updated 15Nov2021
 
 from tkinter import *
 
@@ -12,7 +12,7 @@ class graph_gui:
 
         # Add Labels
         self.label_1 = Label(root,text="Day of Recording (Ex: day1)").place(x=20,y=20)
-        self.label_2 = Label(root,text="Animal Number (Ex: 2207)").place(x=20,y=60)
+        self.label_2 = Label(root,text="Animal Number (Ex: dev2107)").place(x=20,y=60)
 
         # Add Button
         self.btn = Button(root, text='Start Analysis',command=self.save_values).place(x=160,y=110)
@@ -24,9 +24,12 @@ class graph_gui:
         self.entry_2 = Entry(root,width=20,textvariable=self.a_num).place(x=220,y=60)
 
     def save_values(self):
+        global rec_day
+        global ani_num
         rec_day = self.r_day.get()
         ani_num = self.a_num.get()
-        print('Day: '+str(rec_day)+', Rat: '+str(ani_num))
+        print('Analyzing - Day: '+str(rec_day)+', Rat: '+str(ani_num))  
+        self.root.destroy()
 
 root = Tk()
 tnel_gui = graph_gui(root)
