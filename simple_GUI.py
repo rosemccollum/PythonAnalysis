d7756a2796621961fc021d05807671e8484017ad
rae <angelarose.de@gmail.com>
2021-11-16 14:50:21 -0600

    added path name entry

--- simple_GUI.py
+++ simple_GUI.py
@@ -7,28 +7,33 @@ class graph_gui:
         # Setup GUI Window
         self.root = root
         root.title('TNE Lab Coherence vs. Frequency Grapher') # Set Window Name
-        root.geometry('450x160') # Set Width vs. Height
+        root.geometry('450x200') # Set Width vs. Height
         root.resizable(width=False,height=False) # Fix W/H
 
         # Add Labels
         self.label_1 = Label(root,text="Day of Recording (Ex: day1)").place(x=20,y=20)
         self.label_2 = Label(root,text="Animal Number (Ex: dev2107)").place(x=20,y=60)
+        self.label_3 = Label(root, text = "File path (Ex: C:\TNE Lab\RatData)").place(x = 20, y = 100)
 
         # Add Button
-        self.btn = Button(root, text='Start Analysis',command=self.save_values).place(x=160,y=110)
+        self.btn = Button(root, text='Start Analysis',command=self.save_values).place(x=160,y=160)
 
         # Add and configure Entrys
         self.r_day = StringVar()
         self.a_num = StringVar()
+        self.path = StringVar()
         self.entry_1 = Entry(root,width=20,textvariable=self.r_day).place(x=220,y=20)
         self.entry_2 = Entry(root,width=20,textvariable=self.a_num).place(x=220,y=60)
+        self.entry_3 = Entry(root, width = 20, textvariable = self.path).place(x = 220, y = 100)
 
     def save_values(self):
         global rec_day
         global ani_num
+        global path_name
         rec_day = self.r_day.get()
         ani_num = self.a_num.get()
-        print('Analyzing - Day: '+str(rec_day)+', Rat: '+str(ani_num))  
+        path_name = self.path.get()
+        print('Analyzing - Day: ' + str(rec_day) + ', Rat: ' + str(ani_num))  
         self.root.destroy()
 
 root = Tk()
