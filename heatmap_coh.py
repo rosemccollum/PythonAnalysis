51591cd8e555152cd8fddfe1ac0cd984774f860e
rae <angelarose.de@gmail.com>
2021-11-18 16:03:35 -0600

    pulls matlab file & graphs heatmap

--- heatmap_coh.py
+++ heatmap_coh.py
@@ -5,12 +5,36 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# load demo dataset
-df_final = pd.read_csv('heat_demo.csv')
+# Load coh matlab
+coh_mat = scio.loadmat(r'C:\Users\angel\Documents\TNE Lab\Programming\RAW_PRE_dev2107_day2_coh')
+
+# Pull coh, freq, and chan data
+coh = coh_mat['coh_spect']
+freq = coh_mat['freq'][0]
+num_chan = len(coh_mat['cmb_labels'])
+
+# Write into CSV
+filename = 'cohdata.csv'
+i = 0
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(freq)
+    for i in range(len(coh)):
+        csvwriter.writerow(coh[i])
+
+# Load into pandas data frame
+df = pd.read_csv('cohdata.csv',index_col=False)
 
 # Label x and y axis
-y_label = np.arange(1,9,1)
+y_label = np.arange(1, num_chan + 1, 1)
+x_ticks = np.arange(0,61,10)
+x_labels = range(0,31,5)
+
 # Prepare and print Seaborn plot
-plot = sns.heatmap(data =df_final,xticklabels = x_label, yticklabels = y_label, cbar_kws={'label': 'Coherence'},cmap="viridis")
+plot = sns.heatmap(data = df, yticklabels = y_label, cbar_kws={'label': 'Coherence'},cmap="viridis")
+plot.set_xticks(x_ticks)
+plot.set_xticklabels(x_labels)
 plot.set(title="Change in Coherence Spectra", xlabel="Frequency (HZ)", ylabel="Channel cmb #")
+plot.axvline(x=4, color = 'black')
+plot.axvline(x=8, color = 'black')
 plt.show()
