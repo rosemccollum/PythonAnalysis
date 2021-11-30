b1be3d21210cb9e80c0fec4492e1847c3b839cec
rae <angelarose.de@gmail.com>
2021-11-30 15:34:36 -0600

    scatter plot of pre vs delta pwr

---
+++ prepwr_vs_deltapwr_scatter.py
@@ -0,0 +1,104 @@
+# Plot pre pwr vs delta pwr in scatter plot color coordinated by channel with the channel locked channel a bigger dot
+import scipy.io as scio
+import seaborn as sns
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from tkinter import Tk   
+from tkinter.filedialog import askopenfilename
+
+# Choose and open pre and post file
+print('Choose pre data file')
+Tk().withdraw() 
+prepow_name = askopenfilename()
+print("File: ", prepow_name)
+prepow_mat = scio.loadmat(prepow_name)
+
+print('Choose post data file')
+Tk().withdraw() 
+postpow_name = askopenfilename()
+print("File: ", postpow_name)
+postpow_mat = scio.loadmat(postpow_name)
+
+# Pull needed data 
+pre_pow = prepow_mat['powspctrm'] # Goes into power data
+post_pow = postpow_mat['powspctrm']
+freq = prepow_mat['freq'][0]  # Gives list of freq values
+channels_mat = prepow_mat['chan_labels'] # Gives chan labels
+
+# Writes list of channels to use for plotting
+h = 0 
+channels = []
+for g in range(len(channels_mat)):
+    channels.append(channels_mat[g][0][0])
+
+# Writes CSV value w/ pre pwr, no channel lables  
+filename = 'prepowerdata.csv'
+i = 0
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(freq)
+    for i in range(len(pre_pow)):
+        csvwriter.writerow(pre_pow[i])
+
+# Adding chan labels to pre
+temp_df = pd.read_csv(filename)
+temp_df.insert(0, column='freq', value = channels)
+temp_df.to_csv(filename, index = False)
+
+# Writes CSV w/ post pwr, no channel lables 
+filename = 'postpowerdata.csv'
+j = 0
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(freq) # DO WE WANT THIS???
+    for j in range(len(post_pow)):
+        csvwriter.writerow(post_pow[j])
+
+# Adding chan label to post
+temp_df = pd.read_csv(filename)
+temp_df.insert(0, column='freq', value = channels)
+temp_df.to_csv(filename, index = False)
+
+# Load new csvs into Pandas DataFrame
+pre_df = pd.read_csv('prepowerdata.csv',index_col=False)
+post_df = pd.read_csv('postpowerdata.csv',index_col=False)
+
+# Subtract post - pre coh
+delta_df = post_df.set_index('freq').subtract(pre_df.set_index('freq'), fill_value=0)
+
+# Transpose DataFrame
+pre_df = pre_df.T
+delta_df = delta_df.T
+
+# Remove index and save to new DataFrame
+pre_df.columns = pre_df.iloc[0]
+pre_df_fin = pre_df[1:]
+
+# FIXING DELTA DF
+# Remove the frequency data from the index
+delta_df.reset_index(drop = True, inplace = True)
+
+# Drop first column of dataframe
+delta_df = delta_df[[channels[0], channels[1], channels[2], channels[3], channels[4], channels[5], channels[6], channels[7]]]
+
+# Turn into long form data
+delta_melted = pd.melt(delta_df, value_vars=channels, var_name = 'chan', value_name= 'delta')
+
+# FIXING PRE DF
+# Remove the frequency data from the index
+pre_df_fin.reset_index(drop = True, inplace = True)
+
+# Drop first column of dataframe
+pre_df_fin = pre_df_fin[[channels[0], channels[1], channels[2], channels[3], channels[4], channels[5], channels[6], channels[7]]]
+
+# Turn into long form data
+final_df = pd.melt(pre_df_fin, value_vars=channels, var_name = 'chan', value_name= 'pre')
+
+# Adding delta column to pre data 
+delta = delta_melted['delta']
+final_df['delta'] = delta
+
+sns.scatterplot(data = final_df, x = 'pre', y = 'delta', hue = 'chan')
+plt.show()
