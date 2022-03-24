ef9c4ef0b6b29780710b5ced092fdad5d73c474e
rae <angelarose.de@gmail.com>
2022-03-24 14:38:19 -0500

    graphs accel vs time w/ gaussian filter

---
+++ accel_vs_time.py
@@ -0,0 +1,81 @@
+''' Most code borrowed from Jon's gaussian_demo, manipulated to show just accelerometry data. rae McCollum, 17 Mar 22'''    
+import os
+from tkinter.filedialog import askopenfilename
+from urllib.parse import parse_qs
+
+from cv2 import GaussianBlur
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.io as scio
+import scipy.ndimage.filters as filters
+import seaborn as sns
+from tkinter import Tk
+from sklearn import preprocessing
+import math
+
+print("starting...")
+# Load matlab files
+Tk().withdraw() 
+file = askopenfilename()
+print("File: ", file)
+mat = scio.loadmat(file)
+
+# Grab necessary information
+mat_data =  mat['cur_data']['ds_data']
+mat_time = mat['cur_data']['seconds']
+
+cleandata_matlab_struct = mat_data[0][0] 
+
+if len(cleandata_matlab_struct) < 35:
+    print("This file does not contain accelerometry data, please run again with a new file")
+    exit()
+
+# Split file name to get rat and day data
+temp = file.split("/")
+fileName = temp[-1]
+fileName = fileName.split("_")
+for word in fileName:
+    if "dev" in word:
+        rat = word
+    elif "day" in word:
+        day = word
+
+dir = file.split("CLOSED")
+
+# Make dataframe 
+df = pd.DataFrame(cleandata_matlab_struct)
+print("making data frame")
+
+# Reformat DataFrame
+df_final = df.iloc[np.r_[1:2,32:35],:20000] ## Channels 33-35 hold accelerometry data 
+df_final = df_final.T
+    
+df_final.columns = ['lfp','aux1 (x)', 'aux2 (y)', 'aux3 (z)']
+df_final['abs'] = np.sqrt(abs(pow(df_final['aux1 (x)'],2) + pow(df_final['aux2 (y)'],2) + pow(df_final['aux3 (z)'],2)))
+
+# Apply filter 
+gaussian = filters.gaussian_filter1d(df_final['abs'],sigma=15)
+df_final['gaussian'] = gaussian
+df_final_clean = df_final[['abs','gaussian','lfp',]]
+
+# Taking difference of acc data 
+acc = df_final['gaussian']
+delta_list = []
+j = 1
+for i in range(0, len(acc)-1) :
+    delta_list.append(acc[j] - acc[i])
+    j += 1 
+delta_list.append(0)
+df_final['gaussian'] = delta_list
+
+print("graphing...")    
+# Plot results using Seaborn
+ax = sns.lineplot(data=gaussian, color='#EF3D59')
+ax.set_title('Acceleration Vs. Time (s) - {} {}'.format(rat,day))
+ax.set_xlabel('Time (ms)')
+ax.set_ylabel('Acceleration (mV/g)')
+ax.set(xlim =(0, 10000))
+print("done")
+plt.savefig(dir[0] + rat + '_' + day + "_filteredAccel_over_time")
+plt.show()
