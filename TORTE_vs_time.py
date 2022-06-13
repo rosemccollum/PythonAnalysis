9ea9013e495e1ccef24b693e37dce9dc7f28260a
rae McCollum <angelarose.de@gmail.com>
2022-06-13 15:02:32 -0500

    functions graph if no file given

--- TORTE_vs_time.py
+++ TORTE_vs_time.py
@@ -3,95 +3,117 @@ from cv2 import phase
 import matlab.engine 
 import h5py
 import hdf5storage
+import math
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import numpy as np
+from numpy import array
 import pandas as pd
 import scipy.io as scio
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def getTORTE(filename = None):
+    print("torte starting...")
+    # Load matlab files
+    if filename is None:
+        Tk().withdraw() 
+        file = askopenfilename()
+        print("TORTE File: ", file)
+    else :
+        file = filename
+    ### File for debugging
+    ### file = r"C:/Users/angel/Documents/TNELab/Programming/RatData/dev2111/day1/RAW_PRE_dev2111_day1_cleandata_struct.mat"
+    mat = scio.loadmat(file)
 
-print("starting...")
-# Load matlab files
-Tk().withdraw() 
-file = askopenfilename()
-print("File: ", file)
-### File for debugging
-### file = r"C:/Users/angel/Documents/TNELab/Programming/RatData/dev2111/day1/RAW_PRE_dev2111_day1_cleandata_struct.mat"
-mat = scio.loadmat(file)
+    # Grab necessary data
+    mat_phase = mat["cur_data"]["phase_data"]
+    mat_time = mat["cur_data"]["seconds"]
 
-# Grab necessary data
-mat_phase = mat["cur_data"]["phase_data"]
-mat_time = mat["cur_data"]["seconds"]
+    # Make array into list 
+    phase_list = []
+    for i in range(len(mat_phase[0][0][0])) :
+        phase_list.append(mat_phase[0][0][0][i])
 
-# Make array into list 
-phase_list = []
-for i in range(len(mat_phase[0][0][0])) :
-    phase_list.append(mat_phase[0][0][0][i])
+    # Convert to array to correct type, then back to list 
+    phases = np.array(phase_list)
+    phases = phases.astype('int32')
+    phase_list = phases.tolist()
 
-# Convert to array to correct type, then back to list 
-phases = np.array(phase_list)
-phases = phases.astype('int32')
-phase_list = phases.tolist()
+    # Call function
+    print("calling matlab script...")
+    eng = matlab.engine.start_matlab()
+    buff = matlab.double([[200]]) 
+    mat_phase = matlab.double([phase_list])
+    ht_b = matlab.double([[500]])
+    band = matlab.double([[4,8]])
+    Fs = matlab.double([[1000]])
+    upsamp = matlab.double([[True]])
+    torte = eng.hilbert_transformer_phase(mat_phase, buff, ht_b, band, Fs, upsamp, nargout = 3)
+    eng.quit()
 
-# Call function
-print("calling matlab script...")
-eng = matlab.engine.start_matlab()
-buff = matlab.double([[200]]) ## Why am i using 200
-mat_phase = matlab.double([phase_list])
-torte = eng.hilbert_transformer_phase(mat_phase, buff, nargout = 3)
-eng.quit()
+    ## Torte output len is phase_list len / 59.99
 
-## Torte output len is phase_list len / 59.99
+    # Convert to degrees and shorten measurements to every second
+    temp_phases = array(torte[0])
+    d = 0
+    phases = []
+    for d in range(0, len(temp_phases), 1000):
+        temp = float(temp_phases[d])
+        phases.append(math.degrees(temp))
 
-# Shorten torte measurements to every 10
-p = 0
-phases = []
-for p in range(0, len(torte[0]), 10):
-    phases.append(torte[0][p])
+    # Put data in dataframe
+    df = pd.DataFrame(phases)
+    df.rename(columns= {0:'phase'}, inplace=True)
 
-# Put data in dataframe
-df = pd.DataFrame(phases)
-df.rename(columns= {0:'phase'}, inplace=True)
+    # Find mean of phase
+    avg = df['phase'].mean()
+    #avg = sum/len(temp_phases)
 
-# Write and add time values 
-t = 0
-time = []
-for t in range(0, len(mat_time[0][0]), 600):
-    time.append(mat_time[0][0][t][0])
-df["time"] = time 
+    # Write and add time values 
+    t = 0
+    time = []
+    for t in range(0, len(mat_time[0][0]), 1000):
+        time.append(mat_time[0][0][t][0])
+    df["time"] = time 
 
-# Split file name to get rat and day data
-temp = file.split("/")
-fileName = temp[-1]
-fileName = fileName.split("_")
-for word in fileName:
-    if "dev" in word:
-        rat = word
-    elif "day" in word:
-        day = word
-    elif ('POST' in word) or ("PRE" in word):
-        condition = word
-dir = file.split("RAW")
+    # Split file name to get rat and day data
+    temp = file.split("/")
+    fileName = temp[-1]
+    fileName = fileName.split("_")
+    for word in fileName:
+        if "dev" in word:
+            rat = word
+        elif "day" in word:
+            day = word
+        elif ('POST' in word) or ("PRE" in word):
+            condition = word
+    dir = file.split("RAW")
 
-# Determing step for time labels
-if (len(mat_time[0][0]) > 300000):
-    step = 60
-else:
-    step = 1; 
-
-# Graph and save plot 
-print("graphing...")
-sns.set(font_scale = 1.5)
-fig = plt.figure(figsize = (13,7))
-plot = sns.lineplot(data = df, x = 'time', y = 'phase')
-plot.set_title('TORTE Phase over time ' + condition + " " + rat + " " + day)
-plot.set_xlabel('Time (s)')
-plot.set_ylabel('Phase')
-time_labels = (range(0, (len(mat_time[0][0])//1000)+1, step))
-plot.set_xticks(time_labels)
-plot.set_xticklabels(time_labels, rotation = 45)
-plt.savefig(dir[0] + rat + '_' + day + "_" + condition + "_TORTEphase_over_time")
-plt.show()
-print("done")
+    # Determing step for time labels
+    if (len(mat_time[0][0]) > 300000):
+        step = 60
+    else:
+        step = 1; 
+    
+    # End function if passed file name (don't need to graph)
+    if filename is not None:
+        print("done with TORTE calculations")
+        return phases
+    else:
+        # Graph and save plot 
+        print("graphing...")
+        sns.set(font_scale = 1.5)
+        fig = plt.figure(figsize = (13,9))
+        plot = sns.lineplot(data = df, x = 'time', y = 'phase')
+        plot.set_title('TORTE Phase over time ' + condition +  " " + rat + " " + day)
+        plot.set_xlabel('Time (s)')
+        plot.set_ylabel('Phase')
+        plot.set_ylim(-180,180)
+        time_labels = (range(0, (len(mat_time[0][0])//1000)+1, step))
+        plot.set_xticks(time_labels)
+        plot.set_xticklabels(time_labels, rotation = 45)
+        plt.text(20, -170, "Mean value:" + str(avg), horizontalalignment='left', size='medium', color='black', weight='semibold')
+        plt.savefig(dir[0] + rat + '_' + day + "_" + condition + "_TORTEphase_over_time")
+        plt.show()
+        print("done")
--- ground_truth.py
+++ ground_truth.py
@@ -1,5 +1,7 @@
 ''' Calculates ground truth phase'''
 ### Import Dependencies ###
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 from cv2 import phase
 from scipy.signal import hilbert, filtfilt, butter
 from scipy.stats import circmean, circvar
@@ -11,13 +13,22 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
 import scipy.io as scio
 
-''' Calculates GTP and returns single column dataframe of values '''
-def groundTruth(file):
+''' Calculates GTP and returns single column dataframe of values if file given, otherwise graph data '''
+def groundTruth(filename = None):
     print("calculating ground truth phase...")
-    ## file = r"C:\Users\TNEL_Device_8\Documents\raePython\RatData\dev2111\RAW_PRE_dev2111_day1_cleandata_struct.mat"
+   
+    # Load matlab files
+    if filename is None:
+        Tk().withdraw() 
+        file = askopenfilename()
+        print("File: ", file)
+    else :
+        file = filename
+    ### Files for debugging
+    ## file = r"C:\Users\TNEL_Device_8\Documents\raePython\RatData\dev2111\RAW_PRE_dev2111_day1_cleandata_struct.mat" #lab rig
+    ### file = r"C:/Users/angel/Documents/TNELab/Programming/RatData/dev2111/day1/RAW_PRE_dev2111_day1_cleandata_struct.mat" #laptop
     mat = scio.loadmat(file)
 
     # Grab necessary data
@@ -38,16 +49,51 @@ def groundTruth(file):
     filtered_data = filtfilt(b, a, data)
     analytic_data = hilbert(filtered_data)
     phase_data = angle(analytic_data)
-    phase_data = phase_data*180/3.14159
+    phase_data = phase_data*180/3.14159 ## Converting to degrees
     
     # Shorten phase to every second
     phases = []
     p = 0 
-    for p in range(0, len(phase_data)+1, 60):
+    for p in range(0, len(phase_data),1000):
         phases.append(phase_data[p])
 
+    # Write and add time values 
+    t = 0
+    time = []
+    for t in range(0, len(mat_time[0][0]),1000):
+        time.append(mat_time[0][0][t][0])
+
+    # Split file name to get rat and day data
+    temp = file.split("/")
+    fileName = temp[-1]
+    fileName = fileName.split("_")
+    for word in fileName:
+        if "dev" in word:
+            rat = word
+        elif "day" in word:
+            day = word
+        elif ('POST' in word) or ("PRE" in word):
+            condition = word
+        else:
+            condition = "CLOSED"
+    dir = file.split("RAW")
+    
     # Make dataframe
     df = pd.DataFrame(phases)
     df.columns = ['phase_data']
-    return df
+    avg = df['phase_data'].mean()
 
+    if filename is not None:
+        print("done calculating ground truth")
+        return df
+    else:
+        df["time"] = time 
+        print("graphing")
+        sns.set(font_scale = 1.5)
+        fig = plt.figure(figsize = (13,9))
+        plot = sns.lineplot(data = df, x = 'time', y ='phase_data')
+        #plt.savefig(dir[0] + rat + '_' + day + "_" + condition + "_GroundTruthPhase_over_time")
+        plt.text(20, -170, "Mean phase:" +str(avg), horizontalalignment='left', size='medium', color='black', weight='semibold')
+        plt.show()
+        print("done")
+    
